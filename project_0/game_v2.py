aebfd1b30f517f1086cd65729909551ca3b78911
Nekpox <nekpox@gmail.com>
2021-11-06 20:44:18 +0900

    loaded to Github

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,18 +1,49 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
 import numpy as np
 
+
 def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
 
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
     count = 0
-
+    predict_number = np.random.randint(1, 101)
+    start = 1
+    end = 100
+    
+# найдем загаданное число методом деления пополам
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
         if number == predict_number:
-            break  # выход из цикла, если угадали
+            break
+        elif predict_number < number:   # если текущее число меньше загаданного
+            start = predict_number      # обрезаем начало будущих поисков и находим половину разности текущего числа и конца
+            delta = int((end-predict_number)/2)
+            if delta == 0:              # защита от нулевого приращения в случае деления 1 на 2 (при привидении к int будет 0)
+                delta = 1
+            predict_number = predict_number + delta
+            continue
+        elif predict_number > number:   # если текущее число больше загаданного
+            end = predict_number        # обрезаем конец будущих поисков и находим половину разности текущего числа и начала
+            delta = int((predict_number-start)/2)
+            if delta == 0:
+                delta = 1
+            predict_number = predict_number - delta
+            continue
+        
     return count
 
+
 def score_game(random_predict) -> int:
-    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
         random_predict ([type]): функция угадывания
@@ -20,18 +51,18 @@ def score_game(random_predict) -> int:
     Returns:
         int: среднее количество попыток
     """
-
-    count_ls = [] # список для сохранения количества попыток
-    np.random.seed(1) # фиксируем сид для вопроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
 
-    score = int(np.mean(count_ls)) # находим среднее количество попыток
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
 
-    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
-    return(score)
 
-# RUN
-score_game(random_predict)
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
